bc748e61c511003b70c2a6227c14f52d78b77aca
Juan Nicolas Videla <juan.videla@fundingcircle.com>
2020-04-03 06:04:18 +0530

    Working redux store setup with two accounts and basic operations

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import impactReducer from '../pages/second_impact/impactSlice';
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
+    secondImpact: impactReducer
   },
 });
