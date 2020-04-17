31c5477b20fea5cd5e4ec9955582563dd8ee2ded
Juan Nicolas Videla <juan.videla@fundingcircle.com>
2020-04-17 19:17:34 +0530

    Char sheet init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Counter } from './features/counter/Counter';
 import SecondImpact from './pages/second_impact/SecondImpact';
+import Sheet from './pages/char_sheet/sheet';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <SecondImpact/>
+      <Sheet/>
     </div>
   );
 }
---
+++ src/pages/char_sheet/sheet.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const Sheet = () => {
+  return (
+    <div className="character-sheet">
+      <h1>Char Name</h1>
+    </div>
+  )
+}
+
+
+export default Sheet;
