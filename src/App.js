5defb4bf1262f07d756d41bfffec3ffc1edcc5ed
sobre-mesa <sobremesa.software@gmail.com>
2020-04-19 20:48:18 +0530

    idk lol

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Counter } from './features/counter/Counter';
 import SecondImpact from './pages/second_impact/SecondImpact';
 import Sheet from './pages/char_sheet/sheet';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
