952379f537a3c5aa3ac64eda4a40da3701e03d1a
sobre-mesa <sobremesa.software@gmail.com>
2020-04-20 19:53:53 +0530

    Working countdown

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Counter } from './features/counter/Counter';
 import SecondImpact from './pages/second_impact/SecondImpact';
+import PotCook from './pages/pot_cook/potCook';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <SecondImpact/>
+      <PotCook/>
     </div>
   );
 }
