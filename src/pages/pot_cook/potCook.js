fbca4354ad521134c94e1448b41f95d0096c6e94
sobre-mesa <sobremesa.software@gmail.com>
2020-04-20 20:28:42 +0530

    Vegetable threshold implemented, styles

--- src/pages/pot_cook/potCook.js
+++ src/pages/pot_cook/potCook.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import { Tooltip, Progress } from 'antd';
 import { useSelector, useDispatch } from 'react-redux';
-import { set, decrement, selectTotal, selectSecs} from './cookSlice';
+import { set, decrement, selectTotal, selectSecs } from './cookSlice';
 
 const PotCook = () => {
   const dispatch = useDispatch();
   return (
-    <div>
-      <ProgressBar/>
-      <button onClick={() => startTimer(dispatch)}>Hola</button>
+    <div style={{ textAlign: 'center' }}>
+      <ProgressBar />
+      <button style={{ marginTop: '100px' }} onClick={() => startTimer(dispatch)}>Hola</button>
     </div>
   )
 }
@@ -23,9 +23,27 @@ const ProgressBar = (props) => {
   const seconds = useSelector(selectSecs);
   const total = useSelector(selectTotal);
   return (
-    <div>
-      <Tooltip title="3 done / 3 in progress / 4 to do">
-        <Progress percent={(seconds/total) * 100} style={{ width: '60%' }} />
+    <div style={{ marginTop: '200px' }}>
+      <Marker emoji="🍠" seconds="0" total={total} />
+      <Marker emoji="🍖" seconds="20" total={total} />
+      <Progress
+        trailColor="#B7CACD"
+        strokeColor="#9AE2EE"
+        strokeWidth="25px"
+        status={"normal"}
+        showInfo={false}
+        percent={(seconds / total) * 100}
+        style={{ width: "60%" }} />
+    </div>
+  )
+}
+
+const Marker = (props) => {
+  const percentage = 20 + (60 * (props.seconds / props.total)) + '%'
+  return (
+    <div style={{ position: 'absolute', left: percentage , top: 180, zIndex: 10 }}>
+      <Tooltip title={props.seconds}>
+        {props.emoji}
       </Tooltip>
     </div>
   )
