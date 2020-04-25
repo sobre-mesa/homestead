ecf8aae4251b2a14c0aaff126a4c818644d63f6f
sobre-mesa <sobremesa.software@gmail.com>
2020-04-25 14:58:54 +0530

    Google Tab Bar Working

--- src/App.js
+++ src/App.js
@@ -1,57 +1,57 @@
-import React from 'react';
-import { Counter } from './features/counter/Counter';
-import SecondImpact from './pages/second_impact/SecondImpact';
-import PotCook from './pages/pot_cook/potCook';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
-
-import Sheet from './pages/char_sheet/sheet';
 import './App.scss';
 
-const NavBar = props => {
+import React from 'react';
+import { Link, Switch, Route, BrowserRouter } from "react-router-dom";
+import Tab from '@material-ui/core/Tab';
+
+import PotCook from './pages/pot_cook/potCook';
+import SecondImpact from './pages/second_impact/SecondImpact';
+import Sheet from './pages/char_sheet/sheet';
+import Tabs from '@material-ui/core/Tabs';
+
+const routes = [
+  ['/second-impact', 'Second Impact', <SecondImpact />],
+  ['/pot-cook', 'Pot Cook', <PotCook />],
+  ['/character-sheet', 'Character Sheet', <Sheet />],
+  ['/', 'Home', <h1>idk</h1>]]
+
+const tabs = () => {
+  let routesCopy = [...routes];
+  routesCopy.unshift(routesCopy.pop())
+  return routesCopy;
+}
+
+const TabPanel = ({location}) => {
   return (
-      <ul className="nav-bar">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/pot-cook">Pot Cook</Link>
-        </li>
-        <li>
-          <Link to="/second-impact">Second Impact</Link>
-        </li>
-        <li>
-          <Link to="/character-sheet">Life Planner</Link>
-        </li>
-      </ul>
+    <>
+      <Tabs value={location.pathname}>
+        {tabs().map(
+          ([path, label, ]) => (
+            <Tab
+              label={label}
+              value={path}
+              to={path}
+              key={path}
+              component={Link} />))}
+      </Tabs>
+    </>
   )
 }
 
 const App = () => {
   return (
-    <>
-      <Router>
-        <NavBar />
-        <Switch>
-          <Route path="/second-impact">
-            <SecondImpact />
-          </Route>
-          <Route path="/pot-cook">
-            <PotCook />
-          </Route>
-          <Route path="/character-sheet">
-            <Sheet />
-          </Route>
-          <Route path="/">
-            <h1>Idk lol </h1>
-          </Route>
-        </Switch>
-      </Router>
-    </>
+    <BrowserRouter>
+      <Route
+        path="/"
+        render={x => <TabPanel {...x} />}/>
+      <Switch>
+        {routes.map(([path, , component]) => (
+          <Route
+            path={path}
+            key={path}>
+            {component}</Route>))}
+      </Switch>
+    </BrowserRouter>
   );
 }
 
