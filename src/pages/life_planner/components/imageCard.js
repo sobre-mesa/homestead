9eff3dfd63f96ea0c1f56f2edeea10ae073d69f1
sobre-mesa <sobremesa-software@gmail.com>
2020-04-26 17:28:39 +0530

    Implemented card
    Containment logic (Move to backend later)

---
+++ src/pages/life_planner/components/imageCard.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 250,
+  },
+});
+
+
+export default function SectionCard({ name, image, description, children, type, contains }) {
+  const classes = useStyles();
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={image}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {name}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      {contains &&
+        <CardActions>
+          <Button size="small" color="primary">
+            See {contains} ({children.length})
+          </Button>
+          </CardActions>
+        }
+    </Card>
+  );
+}
--- src/pages/life_planner/dev/factory.js
+++ src/pages/life_planner/dev/factory.js
@@ -1,24 +1,24 @@
-let items = [1, 2, 3, 4, 5, 6, 7, 8].map(x => ({
-  id: x,
+let children = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(x => ({
+  depth: 2,
   name: "Some book",
   description: "this book sucksthis book sucksthis book sucksthis book sucksthis book sucksthis book sucksthis book sucksthis book sucks",
   image: "https://i.imgur.com/ESSkOdv.jpeg"
 }));
 
 let webDev = {
-  id: 1,
+  depth: 1,
   name: "Web Development",
   description: "Everything related to Web Development, backend, frontend, it doesnt matter, as long as it helps me build webs, or design them",
   image: "https://www.reachfirst.com/wp-content/uploads/2018/08/Web-Development.jpg",
-  items
+  children
 }
 
 let philosophy = {
-  id: 2,
+  depth: 0,
   name: "Philosophy",
   description: "Never let the future disturb you. You will meet it, if you have to, with the same weapons of reason which today arm you against the present .Everything related to philosophy.",
   image: "https://i.imgur.com/tGeep1V.jpg",
-  items
+  children
 }
 
 const Sections = [webDev, philosophy];
