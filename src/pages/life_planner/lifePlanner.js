433fbda7ef46930880952ac73bc49628a79a86aa
sobre-mesa <sobremesa-software@gmail.com>
2020-04-26 17:28:39 +0530

    Implemented card
    Containment logic (Move to backend later)

--- src/pages/life_planner/lifePlanner.js
+++ src/pages/life_planner/lifePlanner.js
@@ -1,7 +1,31 @@
 import React from 'react';
 import Sections from './dev/factory';
+import SectionCard from './components/imageCard';
 
-console.log(Sections)
+let containment = [["Area", "Areas"],["Subject", "Subjects"], ["Item", "Items"]];
+let typeSingular = depth => {
+  let x = containment[depth];
+  return x ? x[0] : null;
+}
 
-const LifePlanner = () => <h1>Placeholder</h1>
+let typePlural = depth => {
+  let x = containment[depth];
+  return x ? x[1] : null;
+}
+
+let contains = depth => {
+  let x = containment[depth + 1];
+  return x ? x[1] : null;
+};
+
+const LifePlanner = () => {
+  return (
+    <>
+      {Sections.map(x => {
+        return (<SectionCard {...x} type={typeSingular(x.depth)} contains={contains(x.depth)}/>)}
+      )}
+    </>
+  );
+}
+
 export default LifePlanner;
