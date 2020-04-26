63bab577ff35f30f501a95131e084324b00e6d51
sobre-mesa <sobremesa-software@gmail.com>
2020-04-26 21:43:51 +0530

    Moved containment logic to a class

--- src/pages/life_planner/lifePlanner.js
+++ src/pages/life_planner/lifePlanner.js
@@ -1,45 +1,57 @@
 import React from 'react';
+
 import Sections from './dev/factory';
-import SectionCard from './components/imageCard';
+import Containment from './dev/containment';
+
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import { makeStyles } from '@material-ui/core/styles';
 
+import SectionCard from './components/imageCard';
 
-let containment = [["Area", "Areas"], ["Subject", "Subjects"], ["Item", "Items"]];
-let typeSingular = depth => {
-  let x = containment[depth];
-  return x ? x[0] : null;
-}
+let containment = new Containment();
 
-let typePlural = depth => {
-  let x = containment[depth];
-  return x ? x[1] : null;
-}
-
-let contains = depth => {
-  let x = containment[depth + 1];
-  return x ? x[1] : null;
+const gridStyle = {
+  width: '65%',
+  marginLeft: 'auto',
+  marginRight: 'auto',
+  marginTop: 25
 };
+const paperStyle = {
+  marginTop: 50,
+  paddingTop: 50,
+  width: '90%',
+  marginLeft: 'auto',
+  marginRight: 'auto',
+};
+
+
+const useStyles = makeStyles(theme => ({
+  title: {
+    flexGrow: 1,
+    textAlign: 'center',
+  }
+}));
 
 const getCardFromItem = x => {
   return (
     <Grid item xs={6}>
       <SectionCard {...x}
-        type={typeSingular(x.depth)}
-        contains={contains(x.depth)} />
-    </Grid>)
+        type={containment.typeSingular(x.depth)}
+        contains={containment.contains(x.depth)} />
+    </Grid>);
 }
 
 const LifePlanner = () => {
   let cards = Sections.map(getCardFromItem);
-  let cardPerRow = 3;
-  let gridStyle = { width: '65%', marginLeft: 'auto', marginRight: 'auto' };
-  console.log(cards)
+  let classes = useStyles();
   return (
     <>
-      <Paper elevation={15} style={{marginTop: 50,paddingTop: 50,width: '90%', marginLeft: 'auto', marginRight: 'auto' }}>
-      <Typography variant="h2" color="textSecondary" gutterBottom>{typePlural(1)}</Typography>
+      <Paper elevation={15} style={paperStyle}>
+        <Typography variant="h2" color="textPrimary" gutterBottom className={classes.title}>{containment.typePlural(1)}</Typography>
+        <Divider variant="middle" className={classes.divider} />
         <Grid container spacing={6} style={gridStyle}>
           {cards}
         </Grid>
@@ -47,6 +59,5 @@ const LifePlanner = () => {
     </>
   );
 }
-
 
 export default LifePlanner;
